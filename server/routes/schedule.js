cd53c8cc41bde3d6f776c93184d0482e975b6835
PabloCoti <pca2003gte@gmail.com>
2025-05-20 22:13:53 -0600

    Fix movie schedule route missing

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -5,6 +5,7 @@ const scheduleController = require("../controllers/schedule");
 
 router.get("/", scheduleController.listSchedules);
 router.get("/:id", scheduleController.getSchedule);
+router.get('/movie/:movieId', scheduleController.getMovieSchedules);
 
 router.post("/", authenticateToken, scheduleController.createSchedule);
 router.put("/:id", authenticateToken, scheduleController.updateSchedule);
